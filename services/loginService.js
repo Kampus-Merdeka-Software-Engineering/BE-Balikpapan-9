20daa235a35e9a59ac9892faeed0bf96e2aac08f
Pras00 <prasetiawahyu22@gmail.com>
2023-10-04 20:26:28 +0700

    fix

--- services/loginService.js
+++ services/loginService.js
@@ -5,24 +5,24 @@ const bcrypt = require('bcrypt');
 // Autentikasi Login
 async function authentication(usernameOrEmail, plainPassword) {
     try {
-    // Mengecek apakah `usernameOrEmail` adalah username atau email
-    const isEmail = usernameOrEmail.includes('@');
+        // Mengecek apakah `usernameOrEmail` adalah username atau email
+        const isEmail = usernameOrEmail.includes('@');
 
-    const user = await prisma.user.findFirst({
-        where: isEmail ? { email: usernameOrEmail } : { username: usernameOrEmail },
-    });
+        const user = await prisma.user.findFirst({
+            where: isEmail ? { email: usernameOrEmail } : { username: usernameOrEmail },
+        });
 
-    if (user) {
-        const isPasswordMatch = await bcrypt.compare(plainPassword, user.password);
+        if (user) {
+            const isPasswordMatch = await bcrypt.compare(plainPassword, user.password);
 
-        if (isPasswordMatch) {
-            return { success: true, user };
+            if (isPasswordMatch) {
+                return { success: true, user };
+            } else {
+                return { success: false, message: 'Password salah' };
+            }
         } else {
-            return { success: false, message: 'Password salah' };
+            return { success: false, message: 'User (404) Not Found' };
         }
-    } else {
-        return { success: false, message: 'User (404) Not Found' };
-    }
     } catch (error) {
         console.error('Terjadi kesalahan:', error);
         return { success: false, message: 'Terjadi kesalahan saat Autentikasi' };
